f14ed7fcbf414ab60ac59e7065d8b788ca06db55
annis <nissinkentang@gmail.com>
2022-01-22 06:18:02 +0700

    Add project files.

---
+++ ServicePegawaiPAT/IService1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ServicePegawaiPAT
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "Pegawai", ResponseFormat = WebMessageFormat.Json)] //untuk membuat slash, selalu relative
+        List<Pegawai> GetAllPegawai(); //mendapatkan kumpulan Mahasiswa/seluruh data mahasiswa
+        //string GetData(int value);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "Pegawai/id_pegawai={id_pegawai}", ResponseFormat = WebMessageFormat.Json)] //untuk get
+        Pegawai GetPegawaiByid_pegawai(string id_pegawai); //mengambil data berdasarkan nim
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Pegawai", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        string CreatePegawai(Pegawai pgw);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class Pegawai
+    {
+        private string _nama, _id_pegawai, _jk, _agama; //_ adalah Konvensi atau kesepakatan //variabel lokal
+        [DataMember(Order = 1)] //mengirim data untuk mengurutkan //
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+        [DataMember(Order = 2)]
+        public string id_pegawai
+        {
+            get { return _id_pegawai; }
+            set { _id_pegawai = value; }
+        }
+        [DataMember(Order = 3)]
+        public string jenis_kelamin
+        {
+            get { return _jk; }
+            set { _jk = value; }
+        }
+        [DataMember(Order = 4)]
+        public string agama
+        {
+            get { return _agama; }
+            set { _agama = value; }
+        }
+    }
+
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceRest_20190140022_LaelaWulidaImroatusSolikha.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ ServicePegawaiPAT/Service1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ServicePegawaiPAT
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string CreatePegawai(Pegawai pgw)
+        {
+            string msg = "GAGAL";
+            SqlConnection sqlcon = new SqlConnection("Data Source=LAPTOP-07EGJ8M5\\SQLEXPRESS01;Initial Catalog=\"Kelompok\";Persist Security Info=True;User ID=sa;Password=annisa16");
+            string query = String.Format("insert into dbo.Pegawai values('{0}', '{1}','{2}','{3}')", pgw.nama, pgw.id_pegawai, pgw.jenis_kelamin, pgw.agama);
+
+            SqlCommand sqlcom = new SqlCommand(query, sqlcon);
+
+            try
+            {
+                sqlcon.Open();
+                Console.WriteLine(query);
+                sqlcom.ExecuteNonQuery();
+                sqlcon.Close();
+                msg = "Sukses";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+                msg = "Gagal";
+            }
+            return msg;
+        }
+        public List<Pegawai> GetAllPegawai()
+        {
+            List<Pegawai> pegewe = new List<Pegawai>();
+
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-07EGJ8M5\\SQLEXPRESS01;Initial Catalog=\"Kelompok\";Persist Security Info=True;User ID=sa;Password=annisa16");
+            string query = "select nama, id_pegawai, jenis_kelamin, agama from dbo.Pegawai";
+            SqlCommand com = new SqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Pegawai pgw = new Pegawai();
+                    pgw.nama = reader.GetString(0);
+                    pgw.id_pegawai = reader.GetString(1);
+                    pgw.jenis_kelamin = reader.GetString(2);
+                    pgw.agama = reader.GetString(3);
+
+                    pegewe.Add(pgw);
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return pegewe;
+        }
+        public Pegawai GetPegawaiByid_pegawai(string id_pegawai)
+        {
+            Pegawai pgw = new Pegawai();
+            SqlConnection con = new SqlConnection("Data Source=LAPTOP-07EGJ8M5\\SQLEXPRESS01;Initial Catalog=\"Kelompok\";Persist Security Info=True;User ID=sa;Password=annisa16");
+            string query = String.Format("select  nama, id_pegawai, jenis_kelamin, agama from dbo.Pegawai where id_pegawai = '{0}'", id_pegawai);
+            SqlCommand com = new SqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    pgw.nama = reader.GetString(0);
+                    pgw.id_pegawai = reader.GetString(1);
+                    pgw.jenis_kelamin = reader.GetString(2);
+                    pgw.agama = reader.GetString(3);
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return pgw;
+        }
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
